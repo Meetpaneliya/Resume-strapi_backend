88fe4efbbb293231cadc792a49b8f1bd7a53b8cc
Meetpaneliya <meetpaneliya032@gmail.com>
2025-01-21 20:36:29 +0530

    save-3

--- config/health.js
+++ config/health.js
@@ -1,6 +1,28 @@
+// module.exports = {
+//     health: {
+//       path: '/health',
+//       enabled: true,
+//     }
+//   };
+
+
 module.exports = {
+  settings: {
     health: {
       path: '/health',
       enabled: true,
-    }
-  };
+      config: {
+        checks: {
+          database: {
+            enabled: true,
+            responseTime: 1000,
+          },
+          server: {
+            enabled: true,
+            responseTime: 1000,
+          },
+        },
+      },
+    },
+  },
+};
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,36 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
