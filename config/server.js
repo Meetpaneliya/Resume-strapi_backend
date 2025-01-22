9a78110ce401eb69bdb2ac26f7ee6778775d4d07
Meetpaneliya <meetpaneliya032@gmail.com>
2025-01-22 10:14:59 +0530

    save-6

--- config/server.js
+++ config/server.js
@@ -8,4 +8,8 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
   url: env('PUBLIC_URL', 'http://localhost:1337'),
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+    port: 1337, // You can specify the port you want to use
+  },
 });
